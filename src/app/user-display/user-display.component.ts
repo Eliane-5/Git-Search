44b1848bbccb80586b1e0b1d32d5f23fc5b18663
Eliane-5 <irankundaeliane9@gmail.com>
2019-08-29 19:56:27 +0200

    linking our service to our component

--- src/app/user-display/user-display.component.ts
+++ src/app/user-display/user-display.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../user-class/user';
+import { GitRequestService } from '../git-http/git-request.service';
 @Component({
   selector: 'app-user-display',
   templateUrl: './user-display.component.html',
   styleUrls: ['./user-display.component.css']
 })
 export class UserDisplayComponent implements OnInit {
-
-  constructor() { }
+  user:User;
+  constructor(private gitService:GitRequestService) { }
 
   ngOnInit() {
+    this.gitService.gitRequest()
+    this.user = this.gitService.user 
   }
 
 }
