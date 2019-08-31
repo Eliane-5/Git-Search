8c079f99d67828348e41477472e675e72de12278
Eliane-5 <irankundaeliane9@gmail.com>
2019-08-31 18:06:35 +0200

    adding route paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { UserDisplayComponent } from './user-display/user-display.component';
 
+const routes: Routes = [
 
-const routes: Routes = [];
+  { path: 'search', component: SearchFormComponent},
+  { path: 'display/:name', component: UserDisplayComponent },
+  { path: '', redirectTo:"/search", pathMatch:"full"},
+// {path:'User/:id'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
