3200193c963ec0e1d82fcc41aa520889dc008f1e
Eliane-5 <irankundaeliane9@gmail.com>
2019-08-31 18:38:16 +0200

    adding an uppercase first-word pipe

---
+++ src/app/bold.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'bold'
+})
+export class BoldPipe implements PipeTransform {
+
+  transform(value: string, args: any[]): string {
+    if (value === null) return;
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  }
+  }
