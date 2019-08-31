fa119df9fd1ceb63a63f17ef43dc8b918f4283ca
Eliane-5 <irankundaeliane9@gmail.com>
2019-08-31 18:25:55 +0200

    adding module functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserDisplayComponent } from './user-display/user-display.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { BoldPipe } from './bold.pipe';
+import { HoverDirective } from './hover.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDisplayComponent
+    UserDisplayComponent,
+    SearchFormComponent,
+    BoldPipe,
+    HoverDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
