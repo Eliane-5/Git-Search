9c2c52b21e03dd8efe2ea51ff93c6c211355ee76
Eliane-5 <irankundaeliane9@gmail.com>
2019-08-31 18:07:42 +0200

    adding http services with promises

--- src/app/git-http/git-request.service.ts
+++ src/app/git-http/git-request.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { User } from '../user-class/user';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Repositories } from '../repositories';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitRequestService {
   user:User;
-
+  repo: Repositories[];
+  newrepo: Repositories[];
   constructor(private http:HttpClient) {
     this.user = new User("","",0,0,0);
+    this.repo = [];
+    this.newrepo = [];
    }
-   gitRequest(){
+   gitRequest(searchName){
      interface ApiResponse{
       login:string;
       avatar_url:string;
@@ -21,7 +25,7 @@ export class GitRequestService {
       public_repos:number;
      }
      let promise = new Promise((resolve,reject)=>{
-       this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+       this.http.get<ApiResponse>("https://api.github.com/users/"+searchName+"?access_token="+environment.apiUrl).toPromise().then(response=>{
          this.user.login = response.login
          this.user.avatar_url = response.avatar_url
          this.user.followers = response.followers
@@ -35,5 +39,29 @@ export class GitRequestService {
        })
      })
      return promise
+   }
+   gitRepoRequest(searchNameR){
+     interface ApiResponse{
+       name:string;
+       description:string;
+     }
+     let promise = new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>("https://api.github.com/users/"+searchNameR+"/repos?access_token="+environment.apiUrl).toPromise().then(response=>{
+        for(var i in response){
+          if(searchNameR == 'Eliane-5'){
+            this.repo.push(response[i]);
+          }
+          else{
+            this.newrepo.push(response[i])
+          }   
+        }
+        console.log(this.repo)
+        resolve();
+      },error=>{
+        this.user.login = "Please reload the page!";
+        reject(error);
+      })
+     })
+     return promise;
    }
 }
